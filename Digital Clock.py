c72c5f076e98118a8f12e8544d82fa4d6c7c43ca
Kintur Shah <73271742+kinturkt@users.noreply.github.com>
2022-04-21 18:41:47 +0530

    Add files via upload

--- Digital Clock.py
+++ Digital Clock.py
@@ -2,45 +2,33 @@ import time
 import datetime as dt
 import turtle
 
-# create a turtle to display time
 t = turtle.Turtle()
 
-# create a turtle to create rectangle box
 t1 = turtle.Turtle()
 
-# create screen
 s = turtle.Screen()
+s.bgcolor("orange")               # To set the background color
 
-# set background color of the screen
-s.bgcolor("green")
-
-# obtain current hour, minute and second
-# from the system
 sec = dt.datetime.now().second
 min = dt.datetime.now().minute
 hr = dt.datetime.now().hour
 t1.pensize(3)
-t1.color('black')
+t1.color('blue')
 t1.penup()
-
-# set the position of turtle
 t1.goto(-20, 0)
 t1.pendown()
 
-# create rectangular box
 for i in range(2):
     t1.forward(200)
     t1.left(90)
     t1.forward(70)
     t1.left(90)
 
-# hide the turtle
 t1.hideturtle()
 
 while True:
     t.hideturtle()
     t.clear()
-    # display the time
     t.write(str(hr).zfill(2)
             + ":" + str(min).zfill(2) + ":"
             + str(sec).zfill(2),
